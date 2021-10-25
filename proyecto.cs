354a7557e9707e6f1437150bcd7478988fd4655a
36542310Aa <ricardothq@gmail.com>
2021-10-25 09:22:42 -0400

    adicionar primer proyecto

---
+++ proyecto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GIT
+{
+      public class HolaMundo
+      {
+            static void Main(string[] args)
+            {
+                  Console.WriteLine("Hola Mundo!");
+            }
+      }
+}
